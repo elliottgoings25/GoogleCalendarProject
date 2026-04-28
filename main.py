288d6dcaf6ef9fac4a504515be2ea8c5e1fb53fd
elliottgoings25 <elliott.goings@gmail.com>
2026-04-28 09:35:05 -0500

    removed unnecessary stuff on main

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.ui import CalendarUI
-"""
-from fastapi import FastAPI
-from pydantic import BaseModel
-from agent import run_agent 
-"""
-
 
 #--------------------------
 # MAIN ENTRY POINT
@@ -16,23 +10,3 @@ if __name__ == '__main__':
     window = CalendarUI()
     window.show()
     sys.exit(app.exec_())
-"""    
-app = FastAPI()
-
-class Request(BaseModel):
-    message: str
-
-@app.post("/chat")
-def chat(req: Request):
-    result = run_agent(req.message)
-
-    # If Gemini wants to call a function
-    if result["action"] == "create_calendar_event":
-        return {
-            "response": "I can schedule that for you.",
-            "data": result["args"]
-        }
-
-    # Otherwise just return text
-    return {"response": result["text"]}
-"""
